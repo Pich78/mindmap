7fceda0a5977e21b41a967d5e900a7713af01431
Pich78 <apiccianoit@yahoo.it>
2023-11-24 14:40:41 +0100

    Added puml_to_json to convert a plantuml file in json

--- plantuml_to_anytree.py
+++ plantuml_to_anytree.py
@@ -1,49 +1,37 @@
 from anytree import AnyNode, RenderTree
+from anytree.exporter import JsonExporter
+
+def puml_to_json(puml_file_content):
+
+    parent_node = []    #It is the array of the last parent node; the array index is the last level
+    for line in puml_file_content.splitlines():
+        if line != "@startmindmap" and line != "@endmindmap":
+            if line.startswith("*"):
+                node_level = line.count('*')
+                node_name = line.strip('* ').strip()
+
+                if node_level == 1:                        
+                    root = AnyNode(id=node_name)
+                    parent_node.insert(0,root)
+                else:
+                    this_node = AnyNode(id=node_name, parent=parent_node[node_level-2])
+                    parent_node.insert(node_level-1,this_node)  # This is the last node seen at this level
+                    
+
+    exporter = JsonExporter(indent=2, sort_keys=True)
+    return exporter.export(root)
+    #print(RenderTree(root))
+
+
 
 def main():
     filename = "mindmap.puml"
 
-    previous_node_level = 0
-
     with open(filename, "r") as f:
         data = f.read()
-        parent_node = []
-        for line in data.splitlines():
-            if line != "@startmindmap" and line != "@endmindmap":
-                if line.startswith("*"):
-                    node_level = line.count('*')
-                    node_name = line.strip('* ').strip()
+        json = puml_to_json(data)
+        print(json)
 
-                    print("Before")
-                    print(node_level)
-                    print(previous_node_level)
-                    print("------")
-
-                    if node_level == 1:                        
-                        root = AnyNode(id=node_name)
-                        parent_node.insert(0,root)
-                        previous_node_level = 0
-                    else:
-                        this_node = AnyNode(id=node_name, parent=parent_node[previous_node_level])
-                        parent_node.insert(previous_node_level,this_node)
-                        previous_node_level = node_level - 1
-
-                    print("After")
-                    print(node_level)
-                    print(previous_node_level)
-                    print("------")
-
-    
-    #s0 = AnyNode(id="sub0", parent=root)
-    #s0b = AnyNode(id="sub0B", parent=s0, foo=4, bar=109)
-    #s0a = AnyNode(id="sub0A", parent=s0)
-    #s1 = AnyNode(id="sub1", parent=root)
-    #s1a = AnyNode(id="sub1A", parent=s1)
-    #s1b = AnyNode(id="sub1B", parent=s1, bar=8)
-    #s1c = AnyNode(id="sub1C", parent=s1)
-    #s1ca = AnyNode(id="sub1Ca", parent=s1c)    
-    #tree = parse_plantuml_mindmap(filename)
-    print(RenderTree(root))
 
 
 if __name__ == "__main__":
