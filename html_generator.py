960e703a1bcd1ef0a08ec88b9a9117cfb99378de
Pich78 <apiccianoit@yahoo.it>
2023-12-04 14:45:05 +0100

    Generator for encapsulation and library

--- html_generator.py
+++ html_generator.py
@@ -1,20 +1,53 @@
 def generate_html_encapsulation(data):
-    starting_text =  """<!DOCTYPE html>
+    html = """<!DOCTYPE html>
 <html lang="en">
-"""
-    
-    closing_tag = """
-</html>"""
 
-    return (starting_text + data + closing_tag)
+{}
 
+</html>
+    """.format(data)
+    return html
+
+
+
+def generate_head(title, script_url):
+    head = f'''
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{title}</title>
+  <script src="{script_url}"></script>
+  <style>
+    .node {{
+      cursor: pointer;
+    }}
+
+    .node circle {{
+      fill: #fff;
+      stroke: steelblue;
+      stroke-width: 3px;
+    }}
+
+    .node text {{
+      font: 12px sans-serif;
+    }}
+    .link {{
+      fill: none;
+      stroke: #ccc;
+      stroke-width: 2px;
+    }}
+  </style>
+</head>
+'''
+    return head
 
 
 def main():
     filename = "test_html.html"
 
     with open(filename, "w") as f:
-        data = generate_html_encapsulation("")
+        head_data = generate_head("Mindmap", "https://d3js.org/d3.v7.min.js")
+        data = generate_html_encapsulation(head_data)
         f.write(data)
 
 
