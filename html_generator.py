6bf9f2f27721d9f7107e058cc63d5ea06ad6fef3
Pich78 <apiccianoit@yahoo.it>
2023-12-11 14:58:38 +0100

    implemented the behavior in mindmap.py

--- html_generator.py
+++ html_generator.py
@@ -1,4 +1,5 @@
 import json
+from plantuml_to_anytree import puml_to_json
 
 def generate_html_encapsulation(data):
     html = """<!DOCTYPE html>
@@ -48,6 +49,56 @@ def generate_body(json_content):
   <script>
     // Sample JSON data
     const treeData = {json_content};
+  // ************** Generate the tree diagram  *****************
+  var margin = {{top: 20, right: 50, bottom: 20, left: 50}},
+  width = 960 - margin.right - margin.left,
+  height = 500 - margin.top - margin.bottom;
+  
+  var i = 0;
+
+  // Create SVG container
+  const svg = d3.select("body").append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${{margin.left}},${{margin.top}})`);
+
+  // Create hierarchical data structure
+  const root = d3.hierarchy(treeData);
+  var tree = d3.tree().size([height, width]);
+  tree(root);
+
+  // Create links
+  const link = svg.selectAll(".link")
+    .data(root.links())
+    .enter().append("path")
+    .attr("class", "link")
+    .attr("d", d3.linkHorizontal()
+      .x(d => d.y)
+      .y(d => d.x)
+    );
+
+  // Create nodes
+  const node = svg.selectAll(".node")
+    .data(root.descendants())
+    .enter().append("g")
+    .attr("class", "node")
+    .attr("transform", d => `translate(${{d.y}},${{d.x}})`);
+
+  // Debug print of structure
+  console.log("Nodes are: "); 
+  console.log(node); 
+
+  // Add circles to nodes
+  node.append("circle")
+    .attr("r", 10);
+
+  // Add text to nodes
+  node.append("text")
+    .attr("y", -20)
+    .attr("x", -16)
+    .attr("text-anchor", d => ("start"))
+    .text(d => d.data.name);
 
   </script>
 
@@ -57,17 +108,22 @@ def generate_body(json_content):
 
 
 def main():
-    filename = "test_html.html"
+    output_file_name = "test_html.html"
 
-    with open(filename, "w") as f:
+    with open(output_file_name, "w") as output_file:
         head_data = generate_head("Mindmap", "https://d3js.org/d3.v7.min.js")
 
-        with open('mindmap.json', 'r') as file:
-            json_data = json.load(file)
-        json_formatted_data = json.dumps(json_data, indent=4)
-        body_data = generate_body(json_formatted_data)
-        data = generate_html_encapsulation(head_data+body_data)
-        f.write(data)
+        source_file_name = "mindmap.puml"
+        with open(source_file_name, "r") as source_file:
+          data = source_file.read()
+          json = puml_to_json(data)
+
+        #with open('mindmap.json', 'r') as file:
+        #    json_data = json.load(file)
+        #json_formatted_data = json.dumps(json_data, indent=4)
+          body_data = generate_body(json)
+          data = generate_html_encapsulation(head_data+body_data)
+          output_file.write(data)
 
 
 
